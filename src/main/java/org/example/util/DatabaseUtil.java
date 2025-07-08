8e89b2515ba887f8a9c3b8a5bf7ced2eb6b60839
Sebastian Marquez <sebastiancmarquez1998@gmail.com>
2025-07-08 01:09:01 -0500

    Updates database connection URL
    Updates the database connection URL to point to the 'panelsolar' database on the local machine.
    
    This ensures the application connects to the correct database instance.

--- src/main/java/org/example/util/DatabaseUtil.java
+++ src/main/java/org/example/util/DatabaseUtil.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseUtil {
 
-    private static final String URL = "jdbc:mysql:";
+    private static final String URL = "jdbc:mysql://localhost:3306/panelsolar";
     private static final String USER = "usuario";
     private static final String PASSWORD = "password";
 
